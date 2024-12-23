c1388fd15d5730894f395188a0c7ce7ebafe7efe
Alejandro RH <alejandro.rohu@gmail.com>
2024-12-23 11:59:35 -0500

    :sparkles: Add PrimeNG styles and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// PrimeNG configuration
+import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
+// PrimeNG components
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,9 +19,67 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    // PrimeNG
+    CardModule,
+    ButtonModule,
   ],
-  providers: [],
+  providers: [
+    // PrimeNG
+    providePrimeNG({
+      theme: {
+        // Noir
+        preset: definePreset(Aura, {
+          semantic: {
+            primary: {
+              50: '{zinc.50}',
+              100: '{zinc.100}',
+              200: '{zinc.200}',
+              300: '{zinc.300}',
+              400: '{zinc.400}',
+              500: '{zinc.500}',
+              600: '{zinc.600}',
+              700: '{zinc.700}',
+              800: '{zinc.800}',
+              900: '{zinc.900}',
+              950: '{zinc.950}'
+            },
+            colorScheme: {
+              light: {
+                primary: {
+                  color: '{zinc.950}',
+                  inverseColor: '#ffffff',
+                  hoverColor: '{zinc.900}',
+                  activeColor: '{zinc.800}'
+                },
+                highlight: {
+                  background: '{zinc.950}',
+                  focusBackground: '{zinc.700}',
+                  color: '#ffffff',
+                  focusColor: '#ffffff'
+                }
+              },
+              dark: {
+                primary: {
+                  color: '{zinc.50}',
+                  inverseColor: '{zinc.950}',
+                  hoverColor: '{zinc.100}',
+                  activeColor: '{zinc.200}'
+                },
+                highlight: {
+                  background: 'rgba(250, 250, 250, .16)',
+                  focusBackground: 'rgba(250, 250, 250, .24)',
+                  color: 'rgba(255,255,255,.87)',
+                  focusColor: 'rgba(255,255,255,.87)'
+                }
+              }
+            }
+          }
+        }),
+      }
+    })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
